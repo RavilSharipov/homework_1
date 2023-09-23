f467f290207e2298c335bdc443021adfd8d670d5
Ravil <ravilsharipov>
2023-09-24 02:36:19 +0500

    complete 004

---
+++ homework/004/Program.cs
@@ -0,0 +1,14 @@
+﻿// Показывает все четные числа до числа N
+int N, b;
+Console.WriteLine("Введите число");
+N = Convert.ToInt32(Console.ReadLine());
+b = 0;
+while(N > b)
+{
+    b = b + 1;
+    if(b % 2 == 0)
+    {
+        Console.Write("Четное число:");
+        Console.WriteLine(b);
+    }
+}
