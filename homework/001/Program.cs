5a127784676af252b173fb4ecacedfb9929b640f
Ravil <ravilsharipov>
2023-09-24 01:30:48 +0500

    Первая задача

---
+++ homework/001/Program.cs
@@ -0,0 +1,17 @@
+﻿// Вывести на экрна болшее из двух значений
+using System.ComponentModel.DataAnnotations;
+
+int a, b;
+Console.WriteLine("Введите 1-е число");
+a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2-е число");
+b = Convert.ToInt32(Console.ReadLine());
+if(a > b)
+{
+Console.WriteLine("Наибольшее число");
+Console.Write(a);
+} 
+else{
+    Console.WriteLine("Наибольшее число");
+    Console.Write(b);
+}
