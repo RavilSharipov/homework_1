6a6f757e0620267c2ee31a35d8a1b10f4f8bbc6b
Ravil <ravilsharipov>
2023-09-24 02:28:16 +0500

    complete 003

---
+++ homework/003/Program.cs
@@ -0,0 +1,10 @@
+﻿int a;
+Console.WriteLine("Введите число");
+a = Convert.ToInt32(Console.ReadLine());
+if(a % 2 == 0)
+{
+    Console.WriteLine("число четное");
+}
+else{
+    Console.WriteLine("число нечетное");
+}
